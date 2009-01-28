ce1066dd718158318fcd976aa182e493a7dc052e
Warren Cheung <wac@dnahelix.org>
2009-01-28 13:23:34 -0800

    Bugfix for author parsing

--- pubmed_parse/pubmed-baseline-author.py
+++ pubmed_parse/pubmed-baseline-author.py
@@ -19,6 +19,7 @@ for line in sys.stdin:
         else:
             print prevline+sep+"Y"+sep+firstauthorYN
             firstauthorYN='Y'
+            last_pmid=curr_pmid
             
         prevline=line
         
