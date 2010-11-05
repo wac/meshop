4d68a2d39c9a9f55dc1faaf86ffadc5b6e1cc219
Warren Cheung <wcheung@hn1.hpc.oicr.on.ca>
2010-11-05 17:48:40 -0400

    Cleaned up Comments Fixed not parsing the last gene

--- profile_gd_predict/cmp-profile.py
+++ profile_gd_predict/cmp-profile.py
@@ -1,5 +1,3 @@
-# TODO - compute hypergeometric, normalise to pubmed articles (need to get more columns)
-
 import sys
 import math
 
@@ -91,6 +89,96 @@ def main():
     # Process the last one
     process_dterm(currterm, dprofile_raw, dtotal, dprofile_pval, dprofile_tfidf)
 
+def compare_gdterm(currgene, currterm, dprofile_raw, dtotal, dprofile_norm, dprofile_pval, dprofile_tfidf, cosine_norm_dmag, cosine_p_dmag, cosine_tfidf_dmag, gprofile_raw, gtotal, gprofile_norm, gprofile_pval, gprofile_tfidf):
+    cosine_norm_gmag=0.0
+    cosine_p_gmag=0.0
+    cosine_tfidf_gmag=0.0
+
+    # Compute normalised
+    for key in gprofile_raw:
+        gprofile_norm[key]=float(gprofile_raw[key]) / gtotal
+        cosine_norm_gmag=cosine_norm_gmag+(gprofile_norm[key]*gprofile_norm[key])
+        cosine_p_gmag=cosine_p_gmag+(gprofile_pval[key]*gprofile_pval[key])
+        cosine_tfidf_gmag=cosine_tfidf_gmag+(gprofile_tfidf[key]*gprofile_tfidf[key])
+
+    cosine_norm_gmag=math.sqrt(cosine_norm_gmag)
+    cosine_p_gmag=math.sqrt(cosine_p_gmag)
+    cosine_tfidf_gmag=math.sqrt(cosine_tfidf_gmag)
+
+    # Print Profiles
+    pdist_raw=0
+    pdist_norm=0.0
+    pdist_pval=0.0
+    pdist_logpval=0.0
+    ipdist_norm=0.0
+    ipdist_logpval=0.0
+    sumdiff_logp=0.0
+    sum_logcombinedp=0.0
+    
+    cosine_norm=0.0
+    cosine_p=0.0
+    cosine_tfidf=0.0
+            
+    profile_raw=dprofile_raw.copy()
+    profile_norm=dprofile_norm.copy()
+    profile_pval=dprofile_pval.copy()
+    profile_logpval=dprofile_pval.copy()
+    iprofile_logpval = {}
+    iprofile_norm = {}
+
+    for key in profile_logpval:
+        profile_logpval[key]=safelog(profile_logpval[key])
+            
+    for key in gprofile_raw:
+        if key in profile_raw:
+            iprofile_norm[key] = profile_norm[key] - gprofile_norm[key]
+            iprofile_logpval[key] = profile_logpval[key] - safelog(gprofile_pval[key])
+            profile_raw[key] = profile_raw[key] - gprofile_raw[key]
+            profile_norm[key] = profile_norm[key] - gprofile_norm[key]
+            profile_pval[key] = profile_pval[key] - gprofile_pval[key]
+            profile_logpval[key] = profile_logpval[key] - safelog(gprofile_pval[key])
+            cosine_norm = cosine_norm+(dprofile_norm[key]*gprofile_norm[key])
+            cosine_p = cosine_p+(dprofile_pval[key]*gprofile_pval[key])
+            cosine_tfidf = cosine_tfidf+(dprofile_tfidf[key]*gprofile_tfidf[key])
+        else:
+            profile_raw[key] = gprofile_raw[key]
+            profile_norm[key] = gprofile_norm[key]
+            profile_pval[key]= gprofile_pval[key]
+            profile_logpval[key]= safelog(gprofile_pval[key])
+
+    ucount = 0
+    for key in profile_raw:
+        ucount = ucount + 1
+        pdist_raw = pdist_raw + (profile_raw[key]**2)
+        pdist_norm = pdist_norm + (profile_norm[key]**2)
+        pdist_pval = pdist_pval + (profile_pval[key]**2)
+        pdist_logpval = pdist_logpval + (profile_logpval[key]**2)
+        sumdiff_logp = sumdiff_logp + abs(profile_logpval[key])
+
+    icount = 0
+    for key in iprofile_logpval:
+        icount = icount + 1
+        ipdist_norm=ipdist_norm + (iprofile_norm[key]**2)
+        ipdist_logpval=ipdist_logpval + (iprofile_logpval[key]**2)
+        sum_logcombinedp = sum_logcombinedp + safelog(dprofile_pval[key] + gprofile_pval[key] - (dprofile_pval[key] * gprofile_pval[key]))
+
+    pdist_raw = pdist_raw ** (0.5)
+    # Max dist is 2.0
+    pdist_norm = (pdist_norm ** (0.5)) / 2.0
+    pdist_pval = pdist_pval ** (0.5)
+    pdist_logpval = pdist_logpval ** (0.5)
+
+    cosine_norm = cosine_norm/( cosine_norm_gmag * cosine_norm_dmag )
+    if ( cosine_p_gmag * cosine_p_dmag == 0):
+        sys.stderr.write("Profile computation Error ("+currterm+sep+currgene+"): cosine_p_gmag ("+str(cosine_p_gmag)+") * cosine_p_dmag ("+str(cosine_p_dmag)+") == 0\n")
+        sys.exit(1)
+
+    cosine_p = cosine_p/( cosine_p_gmag * cosine_p_dmag )
+    cosine_tfidf = cosine_tfidf/( cosine_tfidf_gmag * cosine_tfidf_dmag )
+
+    print currterm+sep+currgene+sep+str(len(dprofile_raw))+sep+str(len(gprofile_raw))+sep+str(icount)+sep+str(ucount)+sep+str(pdist_raw)+sep+str(pdist_norm)+sep+str(pdist_pval)+sep+str(pdist_logpval)+sep+str(ipdist_norm)+sep+str(ipdist_logpval)+sep+str(sumdiff_logp)+sep+str(sum_logcombinedp)+sep+str(cosine_norm)+sep+str(cosine_p)+sep+str(cosine_tfidf)
+
+            
 def process_dterm(currterm, dprofile_raw, dtotal, dprofile_pval, dprofile_tfidf):
     global sep
             
@@ -134,92 +222,7 @@ def process_dterm(currterm, dprofile_raw, dtotal, dprofile_pval, dprofile_tfidf)
             currgene=gene
 
         if not(gene==currgene):
-            cosine_norm_gmag=0.0
-            cosine_p_gmag=0.0
-            cosine_tfidf_gmag=0.0
-
-            # Compute normalised
-            for key in gprofile_raw:
-                gprofile_norm[key]=float(gprofile_raw[key]) / gtotal
-                cosine_norm_gmag=cosine_norm_gmag+(gprofile_norm[key]*gprofile_norm[key])
-                cosine_p_gmag=cosine_p_gmag+(gprofile_pval[key]*gprofile_pval[key])
-                cosine_tfidf_gmag=cosine_tfidf_gmag+(gprofile_tfidf[key]*gprofile_tfidf[key])
-
-            cosine_norm_gmag=math.sqrt(cosine_norm_gmag)
-            cosine_p_gmag=math.sqrt(cosine_p_gmag)
-            cosine_tfidf_gmag=math.sqrt(cosine_tfidf_gmag)
-
-            # Print Profiles
-            pdist_raw=0
-            pdist_norm=0.0
-            pdist_pval=0.0
-            pdist_logpval=0.0
-            ipdist_norm=0.0
-            ipdist_logpval=0.0
-            sumdiff_logp=0.0
-            sum_logcombinedp=0.0
-
-            cosine_norm=0.0
-            cosine_p=0.0
-            cosine_tfidf=0.0
-
-            profile_raw=dprofile_raw.copy()
-            profile_norm=dprofile_norm.copy()
-            profile_pval=dprofile_pval.copy()
-            profile_logpval=dprofile_pval.copy()
-            iprofile_logpval = {}
-            iprofile_norm = {}
-
-            for key in profile_logpval:
-                profile_logpval[key]=safelog(profile_logpval[key])
-            
-            for key in gprofile_raw:
-                if key in profile_raw:
-                    iprofile_norm[key] = profile_norm[key] - gprofile_norm[key]
-                    iprofile_logpval[key] = profile_logpval[key] - safelog(gprofile_pval[key])
-                    profile_raw[key] = profile_raw[key] - gprofile_raw[key]
-                    profile_norm[key] = profile_norm[key] - gprofile_norm[key]
-                    profile_pval[key] = profile_pval[key] - gprofile_pval[key]
-                    profile_logpval[key] = profile_logpval[key] - safelog(gprofile_pval[key])
-                    cosine_norm = cosine_norm+(dprofile_norm[key]*gprofile_norm[key])
-                    cosine_p = cosine_p+(dprofile_pval[key]*gprofile_pval[key])
-                    cosine_tfidf = cosine_tfidf+(dprofile_tfidf[key]*gprofile_tfidf[key])
-                else:
-                    profile_raw[key] = gprofile_raw[key]
-                    profile_norm[key] = gprofile_norm[key]
-                    profile_pval[key]= gprofile_pval[key]
-                    profile_logpval[key]= safelog(gprofile_pval[key])
-
-            ucount = 0
-            for key in profile_raw:
-                ucount = ucount + 1
-                pdist_raw = pdist_raw + (profile_raw[key]**2)
-                pdist_norm = pdist_norm + (profile_norm[key]**2)
-                pdist_pval = pdist_pval + (profile_pval[key]**2)
-                pdist_logpval = pdist_logpval + (profile_logpval[key]**2)
-                sumdiff_logp = sumdiff_logp + abs(profile_logpval[key])
-
-            icount = 0
-            for key in iprofile_logpval:
-                icount = icount + 1
-                ipdist_norm=ipdist_norm + (iprofile_norm[key]**2)
-                ipdist_logpval=ipdist_logpval + (iprofile_logpval[key]**2)
-                sum_logcombinedp = sum_logcombinedp + safelog(dprofile_pval[key] + gprofile_pval[key] - (dprofile_pval[key] * gprofile_pval[key]))
-
-            pdist_raw = pdist_raw ** (0.5)
-            # Max dist is 2.0
-            pdist_norm = (pdist_norm ** (0.5)) / 2.0
-            pdist_pval = pdist_pval ** (0.5)
-            pdist_logpval = pdist_logpval ** (0.5)
-
-            cosine_norm = cosine_norm/( cosine_norm_gmag * cosine_norm_dmag )
-            if ( cosine_p_gmag * cosine_p_dmag == 0):
-                sys.stderr.write("Profile computation Error ("+currterm+sep+currgene+"): cosine_p_gmag ("+str(cosine_p_gmag)+") * cosine_p_dmag ("+str(cosine_p_dmag)+") == 0\n")
-                sys.exit(1)
-            cosine_p = cosine_p/( cosine_p_gmag * cosine_p_dmag )
-            cosine_tfidf = cosine_tfidf/( cosine_tfidf_gmag * cosine_tfidf_dmag )
-
-            print currterm+sep+currgene+sep+str(len(dprofile_raw))+sep+str(len(gprofile_raw))+sep+str(icount)+sep+str(ucount)+sep+str(pdist_raw)+sep+str(pdist_norm)+sep+str(pdist_pval)+sep+str(pdist_logpval)+sep+str(ipdist_norm)+sep+str(ipdist_logpval)+sep+str(sumdiff_logp)+sep+str(sum_logcombinedp)+sep+str(cosine_norm)+sep+str(cosine_p)+sep+str(cosine_tfidf)
+            compare_gdterm(currgene, currterm, dprofile_raw, dtotal, dprofile_norm, dprofile_pval, dprofile_tfidf, cosine_norm_dmag, cosine_p_dmag, cosine_tfidf_dmag, gprofile_raw, gtotal, gprofile_norm, gprofile_pval, gprofile_tfidf)
             
             # Reset gene profile
             currgene=gene
@@ -234,18 +237,7 @@ def process_dterm(currterm, dprofile_raw, dtotal, dprofile_pval, dprofile_tfidf)
         gprofile_pval[gterm]=gpval
         gprofile_tfidf[gterm]=gtfidf
     gfile.close()
-
+    # Process the last gene
+    compare_gdterm(currgene, currterm, dprofile_raw, dtotal, dprofile_norm, dprofile_pval, dprofile_tfidf, cosine_norm_dmag, cosine_p_dmag, cosine_tfidf_dmag, gprofile_raw, gtotal, gprofile_norm, gprofile_pval, gprofile_tfidf)
 main()
 
-# Read profile into hash
-# Create Normalised profile 
-
-# Open Gene Profile files
-
-# For Each gene profile
-
-# copy disease profiles
-
-# Read the gene profile
-# For each term,  subtract from hash for each term
-# Then,  compute distances
