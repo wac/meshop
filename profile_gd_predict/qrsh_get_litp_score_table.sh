8731c83f593a796f0aec5e77add0cda6c1bcefe1
Warren Cheung <wcheung@cn3-49.hpc.oicr.on.ca>
2011-12-11 05:02:23 +0000

    Made score to compute litp table generic (score X rather than score 8)

--- profile_gd_predict/qrsh_get_litp_score_table.sh
+++ profile_gd_predict/qrsh_get_litp_score_table.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -v PATH
+#$ -l h_vmem=60G
+#$ -l h_stack=256M
+#
+
 
 TXTDIR=../txt/profile_gene_disease
+#SCORE=8
+SCORE=12
 
-if [ -f $TXTDIR/disease-pharma-chem-litp-profiles.txt ]
+if [ -f $TXTDIR/disease-pharma-chem-litp-score$SCORE-profiles.txt ]
 then
-    echo Found -- $TXTDIR/disease-pharma-chem-litp-profiles.txt
+    echo Found -- $TXTDIR/disease-pharma-chem-litp-$SCORE-profiles.txt
     echo done
     exit
 fi
-
 echo Not Found -- $TXTDIR/disease-pharma-chem-litp-profiles.txt
 
-
-if [ ! -f $TXTDIR/disease-pharma-chem-profiles-score3,4,10.txt ]
+if [ ! -f $TXTDIR/disease-pharma-chem-profiles-score3,4,$SCORE.txt ]
 then
-    cat $TXTDIR/disease-pharma-chem-profiles.txt | grep -E -v "^#" | cut -f 3,4,10 -d "|"  > $TXTDIR/disease-pharma-chem-profiles-score3,4,10.txt.tmp && mv $TXTDIR/disease-pharma-chem-profiles-score3,4,10.txt.tmp $TXTDIR/disease-pharma-chem-profiles-score3,4,10.txt
-    exit
+    echo Not Found -- $TXTDIR/disease-pharma-chem-profiles-score3,4,$SCORE.txt
+    cat $TXTDIR/disease-pharma-chem-profiles.txt | grep -E -v "^#" | cut -f 3,4,$SCORE -d "|"  > $TXTDIR/disease-pharma-chem-profiles-score3,4,$SCORE.txt.tmp && mv $TXTDIR/disease-pharma-chem-profiles-score3,4,$SCORE.txt.tmp $TXTDIR/disease-pharma-chem-profiles-score3,4,$SCORE.txt
+    echo Generated -- $TXTDIR/disease-pharma-chem-profiles-score3,4,$SCORE.txt
+    rm -f $TXTDIR/disease-pharma-chem-litp-score_table.*.*.txt
+    echo "Cleared $TXTDIR/disease-pharma-chem-litp-score_table.*.*.txt"
 fi
+
+if [ ! -f $TXTDIR/disease-pharma-score$SCORE-chem-pscores.txt ]
+then
+    echo Running get_lit_pscores.sh $TXTDIR $SCORE
+    sh get_lit_pscores.sh $TXTDIR $SCORE
+fi
+
+rm -f "$TXTDIR/disease-pharma-chem-profiles-score3,4,X.txt"
+ln -s "disease-pharma-chem-profiles-score3,4,$SCORE.txt" "$TXTDIR/disease-pharma-chem-profiles-score3,4,X.txt"
 
 ALLOK=Y
 for i in {1..100}
@@ -38,16 +59,15 @@ then
     exit
 fi
 
-if [ ! -f $TXTDIR/disease-pharma-chem-litp-score_table.txt ]
+if [ ! -f $TXTDIR/disease-pharma-chem-litp-score$SCORE-table.txt ]
 then
-    echo All files present -- Generating $TXTDIR/disease-pharma-chem-litp-score_table.txt
-    echo Generating $TXTDIR/disease-pharma-chem-litp-score_table.txt
-    cat $TXTDIR/disease-pharma-chem-litp-score_table.*.*.txt > $TXTDIR/disease-pharma-chem-litp-score_table.txt.tmp && mv $TXTDIR/disease-pharma-chem-litp-score_table.txt.tmp $TXTDIR/disease-pharma-chem-litp-score_table.txt || exit
+    echo All files present -- Generating $TXTDIR/disease-pharma-chem-litp-score$SCORE-table.txt
+    cat $TXTDIR/disease-pharma-chem-litp-score_table.*.*.txt > $TXTDIR/disease-pharma-chem-litp-score$SCORE-table.txt.tmp && mv $TXTDIR/disease-pharma-chem-litp-score$SCORE-table.txt.tmp $TXTDIR/disease-pharma-chem-litp-score$SCORE-table.txt || exit
     echo done
 fi
 
-echo Generating $TXTDIR/disease-pharma-chem-litp-profiles.txt
+echo Generating $TXTDIR/disease-pharma-chem-litp-score$SCORE-profiles.txt
 
-cat $TXTDIR/disease-pharma-chem-profiles.txt | cut -f 1,2,3,4,10 -d '|' | python merge-litp-score2.py $TXTDIR/disease-pharma-chem-pscores.txt $TXTDIR/disease-pharma-chem-litp-score_table.txt > $TXTDIR/disease-pharma-chem-litp-profiles.txt.tmp && mv $TXTDIR/disease-pharma-chem-litp-profiles.txt.tmp $TXTDIR/disease-pharma-chem-litp-profiles.txt
+cat $TXTDIR/disease-pharma-chem-profiles.txt | cut -f 1,2,3,4,$SCORE -d '|' | python merge-litp-score2.py $TXTDIR/disease-pharma-score$SCORE-chem-pscores.txt $TXTDIR/disease-pharma-chem-litp-score$SCORE-table.txt > $TXTDIR/disease-pharma-chem-litp-score$SCORE-profiles.txt.tmp && mv $TXTDIR/disease-pharma-chem-litp-score$SCORE-profiles.txt.tmp $TXTDIR/disease-pharma-chem-litp-score$SCORE-profiles.txt
 
 echo done
